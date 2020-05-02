241edc8b1c77138c6eec2b7ab45e69e92ae356a9
Blackhawk-TA <tkcblackhawk@gmail.com>
2020-05-02 13:39:14 +0200

    Adds unit tests

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Apple-Music-Playlist
+[![license](http://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/Blackhawk-TA/apple-music-playlist/blob/master/LICENSE.md)
+[![github](https://img.shields.io/github/package-json/v/Blackhawk-TA/apple-music-playlist.svg?color=brightgreen)](https://github.com/Blackhawk-TA/apple-music-playlist/tree/master)
+[![npm](https://img.shields.io/npm/v/apple-music-playlist.svg?color=brightgreen)](https://www.npmjs.com/package/apple-music-playlist)
+[![Build Status](https://travis-ci.com/Blackhawk-TA/apple-music-playlist.svg?branch=master)](https://travis-ci.com/Blackhawk-TA/apple-music-playlist)
+[![Coverage Status](https://coveralls.io/repos/github/Blackhawk-TA/apple-music-playlist/badge.svg?branch=master)](https://coveralls.io/github/Blackhawk-TA/apple-music-playlist?branch=master)
+
 ### What is Apple-Music-Playlist?
 It is a small npm module which allows getting the playlist information of an apple music playlist by its URL.
 The informations contains the title, artist and album of each track.
---
+++ test/main.spec.js
@@ -0,0 +1,50 @@
+const amply = require("./../src/main");
+const assert = require("assert");
+
+describe("The main module", function() {
+	describe("The function getPlaylist", function() {
+		it("Resolves the promise with the album, artist and title in an array as result", function() {
+			//Arrange
+			let sPlaylistUrl = "https://music.apple.com/de/playlist/test/pl.u-RRbV0XNsxa2oq6",
+				aExpectedResult = [{
+					album: "This Is War (feat. Richard Farrell) - Single",
+					artist: "Matthew Raetzel",
+					title: "This Is War (feat. Richard Farrell)"
+				}, {
+					album: "Inception (Music from the Motion Picture)",
+					artist: "Hans Zimmer",
+					title: "Time"
+				}];
+
+			//Act
+			return amply.getPlaylist(sPlaylistUrl).then(aResult => {
+				//Assert
+				assert.deepStrictEqual(aResult, aExpectedResult, "The returned playlist is correct.");
+			});
+		});
+
+		it("Resolves the promise with an empty array if the url is incorrect", function() {
+			//Arrange
+			let sPlaylistUrl = "https://music.apple.com/wrong-playist-ul",
+				aExpectedResult = [];
+
+			//Act
+			return amply.getPlaylist(sPlaylistUrl).then(aResult => {
+				//Assert
+				assert.deepStrictEqual(aResult, aExpectedResult, "The returned playlist is empty.");
+			});
+		});
+
+		it("Rejects the promise with an error if the url is empty", function() {
+			//Arrange
+			let sPlaylistUrl = "",
+				sExpectedError = "Error: options.uri is a required argument";
+
+			//Act
+			return amply.getPlaylist(sPlaylistUrl).catch(err => {
+				//Assert
+				assert.strictEqual(err.toString(), sExpectedError, "The correct error message is returned.");
+			});
+		});
+	});
+});
